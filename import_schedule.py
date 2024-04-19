28ba4cd824f885507080f12a99b106e2932ce7fa
avdivo <avdivo@mail.ru>
2024-04-20 00:59:21 +0300

    Получен первый результат

--- import_schedule.py
+++ import_schedule.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import re
 import time
 
 # Запускаем браузер
@@ -15,7 +16,7 @@ page_source = driver.page_source
 # Выводим содержимое страницы
 
 # Находим все элементы <div> с классом "bus-route-title"
-buses = driver.find_elements(By.XPATH, '//div[@class="bus-route-title"]')
+buses = driver.find_elements(By.XPATH, '//div[@class="bus-route-container"]')
 
 """Словарь со структурой 
 {   
@@ -27,79 +28,148 @@ buses = driver.find_elements(By.XPATH, '//div[@class="bus-route-title"]')
 }
 """
 all_schedule = dict()
-
+path_in_dict = all_schedule  # Тут словарь (ветка) словаря с которым работаем
 
 # Это спойлеры маршрутов, открывая которые получаем расписание.
 # В нем первая кнопка для открытия модального окна с расписанием по остановкам.
 # Могут быть кнопки дополнительных маршрутов,
 # открывающие модальные окна с их расписаниям по остановкам
 for bus in buses:
-    bus.click()
-    # Получаем все кнопки
-    modals = driver.find_elements(By.XPATH, '//div[@class="by-points"]')
+    bus_numer = number = re.search(r'№\s*([^ ]+)', bus.text).group(1)
+    bus.click()  # Открываем спойлер
     time.sleep(0.2)
+    modals = bus.find_elements(By.CSS_SELECTOR, ".by-points")
 
-
+    # Разбираем содержимое спойлера
+    # Список кнопок открывающих модальные окна
+    # Первая кнопка - расписание для основного маршрута
+    one = True
     for modal in modals:
-        if not modal.text:
-            continue
-        print(modal.text)
-        modal.click()
+        if one:
+            # Если это переход к окну расписания основного маршрута (первая кнопка), то номер автобуса мы уже имеем
+            one = False
+        else:
+            # Получаем номер дополнительного автобуса
+            bus_numer = number = re.search(r'№\s*([^ ]+)', modal.text).group(1)
+
+        path_in_dict[bus_numer] = dict()  # Добавляем словарь для маршрута в основной словарь
+        path_in_dict = path_in_dict[bus_numer]  # Теперь работаем с вложенным словарем в рамках сохраненного маршрута
+
+        modal.click()  # Открываем модальное окно
         time.sleep(1)
-#
+
         # Находим элемент модального окна с помощью CSS-селектора
         modal_elements = driver.find_elements(By.CSS_SELECTOR, "#modal-container")
-#
-#         # Получаем содержимое модального окна
-#         modal_content = ""
-#         for element1 in modal_elements:
-#             modal_content += element1.text
-# #
-#         # Выводим содержимое модального окна на экран
-#         print(modal_content)
-#
+
+        # Проверяем, был ли найден элемент модального окна
+        if not modal_elements:
+            continue
 
         # Получение списка спойлеров маршрутов (конечная-конечная)
         containers = modal_elements[0].find_elements(By.CSS_SELECTOR, "[class*='route-'][class$='container']")
-        # Получение остановок на маршрутах
-        bus_points = modal_elements[0].find_elements(By.CSS_SELECTOR, "[class='bus-point']")
-        print(len(bus_points))
-        print(containers)
+
         for container in containers:
-            print(container.text)
-            container.click()
-            for container in bus_points:
-                print(container.text)
+            path_in_dict[container.text] = dict()  # Добавляем словарь для направления в словарь маршрута
+            path_in_dict = path_in_dict[container.text]  # Теперь работаем с вложенным словарем
+            container.click()  # Открываем спойлер
+            time.sleep(0.2)
+
+            bus_points = container.find_elements(By.CSS_SELECTOR, ".bus-point")  # Под спойлером список остановок
 
 
+            for bus_point in bus_points:
+                print(bus_points)
+                direction = re.sub(r'[^\w\s]', '', bus_point.text)  # Очищаем текст
+                print(bus_point.text)
 
-        # Проверяем, был ли найден элемент модального окна
-        if len(modal_elements) > 0:
-            # Находим кнопки-спойлеры открывающие список остановок
+                path_in_dict[direction] = dict()  # Добавляем словари остановок в словарь направления
+                path_in_dict = path_in_dict[direction]  # Теперь работаем с вложенным словарем
 
-            # Найдем элемент закрытия модального окна
-            close_button = modal_elements[0].find_element(By.ID, "modal-close")
-            # Нажать на элемент закрытия модального окна
-            close_button.click()
+                modal_contents = modal_elements[0].find_element(By.XPATH, "//div[@id='modal-container']").get_attribute("outerHTML")
+
+                bus_point.click()  # Переходим к расписанию
+                time.sleep(0.5)
+
+                print(modal_elements[0].text)
+
+                # Восстанавливаем содержимое модального окна
+                driver.execute_script('''
+                    var modal = document.createElement("div");
+                    modal.innerHTML = arguments[0];
+                    var modalContainer = document.getElementById("modal-container");
+                    modalContainer.innerHTML = "";
+                    modalContainer.appendChild(modal);
+                ''', modal_contents)
+
+                bus_points = driver.find_elements(By.CSS_SELECTOR, ".bus-point")  # Под спойлером список остановок
+                print(bus_points[0].text)
+            # print(all_schedule)
+exit()
+#     # Получаем все кнопки
+#     modals = driver.find_elements(By.XPATH, '//div[@class="by-points"]')
+#     time.sleep(0.2)
 #
-        time.sleep(0.2)
-
-    bus.click()
-
-
-
-# Открываем каждый спойлер, находим вложенные элементы и распечатываем текст
-# for spoiler in spoilers:
-#     spoiler.click()  # Кликаем по спойлеру
-#     # time.sleep(1)  # Ждем некоторое время, чтобы контент успел загрузиться
 #
-#     nested_elements = spoiler.find_elements("xpath", "./following-sibling::*")
-#     for element in nested_elements:
-#         html_content = element.get_attribute("outerHTML")
-#         print(html_content)
-
-
-# Закрываем браузер
-driver.quit()
+#     for modal in modals:
+#         if not modal.text:
+#             continue
+#         print(modal.text)
+#         modal.click()
+#         time.sleep(1)
+# #
+#         # Находим элемент модального окна с помощью CSS-селектора
+#         modal_elements = driver.find_elements(By.CSS_SELECTOR, "#modal-container")
+# #
+# #         # Получаем содержимое модального окна
+# #         modal_content = ""
+# #         for element1 in modal_elements:
+# #             modal_content += element1.text
+# # #
+# #         # Выводим содержимое модального окна на экран
+# #         print(modal_content)
+# #
+#
+#         # Получение списка спойлеров маршрутов (конечная-конечная)
+#         containers = modal_elements[0].find_elements(By.CSS_SELECTOR, "[class*='route-'][class$='container']")
+#         # Получение остановок на маршрутах
+#         bus_points = modal_elements[0].find_elements(By.CSS_SELECTOR, "[class='bus-point']")
+#         print(len(bus_points))
+#         print(containers)
+#         for container in containers:
+#             print(container.text)
+#             container.click()
+#             for container in bus_points:
+#                 print(container.text)
+#
+#
+#
+#         # Проверяем, был ли найден элемент модального окна
+#         if len(modal_elements) > 0:
+#             # Находим кнопки-спойлеры открывающие список остановок
+#
+#             # Найдем элемент закрытия модального окна
+#             close_button = modal_elements[0].find_element(By.ID, "modal-close")
+#             # Нажать на элемент закрытия модального окна
+#             close_button.click()
+# #
+#         time.sleep(0.2)
+#
+#     bus.click()
+#
+#
+#
+# # Открываем каждый спойлер, находим вложенные элементы и распечатываем текст
+# # for spoiler in spoilers:
+# #     spoiler.click()  # Кликаем по спойлеру
+# #     # time.sleep(1)  # Ждем некоторое время, чтобы контент успел загрузиться
+# #
+# #     nested_elements = spoiler.find_elements("xpath", "./following-sibling::*")
+# #     for element in nested_elements:
+# #         html_content = element.get_attribute("outerHTML")
+# #         print(html_content)
+#
+#
+# # Закрываем браузер
+# driver.quit()
 
 
---
+++ import_schedule1.py
@@ -0,0 +1,177 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re, os, json
+from time import sleep
+from datetime import time
+
+
+def save_result(result: dict):
+    """Сохраняет результат в файл."""
+    with open('result.json', "w") as file:
+        json.dump(result, file, ensure_ascii=False, indent=4)
+
+
+def final_station(string):
+    """Создает списки конечных остановок и Направлений.
+    Принимает список направлений (через тире), создает список из названий отделенных тире.
+    Каждый раз сохраняет списки.
+    """
+    data_final = []
+    file_final = 'final.json'
+    if os.path.exists(file_final):
+        with open(file_final, "r") as file:
+            data_final = json.load(file)
+    data_direction = []
+    file_direction = 'direction.json'
+    if os.path.exists(file_direction):
+        with open(file_direction, "r") as file:
+            data_direction = json.load(file)
+
+    bs_list = []
+    dir_list = []
+    for s in string:
+        if not s:
+            continue
+        dir_list.append(s)
+        for bs in s.split(' - '):
+            out = bs.strip()
+            if out:
+                bs_list.append(out)
+
+    data = set(data_final).union(set(bs_list))
+    with open(file_final, "w") as file:
+        json.dump(list(data), file, ensure_ascii=False, indent=4)
+    data = set(data_direction).union(set(dir_list))
+    with open(file_direction, "w") as file:
+        json.dump(list(data), file, ensure_ascii=False, indent=4)
+
+
+def get_schedule():
+    """Получение расписания.
+    Возвращает список с текстовым расписанием"""
+    table = driver.find_elements(By.XPATH, '//*[@id="schedule"]')  # Чтение таблицы
+    hours, minits = 0, 0
+    schedule = []
+
+    # Значения времени разделены переносами строки и пробелами
+    # В первой строке заголовки. Часы записаны с двоеточием
+    # После часов идут значения минут (одно или несколько, для этого часа)
+    try:
+        strings = table[0].text.split()  # Разделяем на цифры по пробелам и переносам
+    except:
+        print("Не удалось распознать таблицу.")
+        return {}
+
+    for string in strings:
+        number = re.findall(r'\d+', string)  # Получаем из строки число, если оно там есть
+        if not number:  # Если числа нет - пропускаем
+            continue
+        if ':' in string:
+            hours = number[0]  # Если в строке есть двоеточие - то это значение часов
+        else:
+            # Если просто число - это минуты
+            schedule.append(f'{hours}:{number[0]}')  # Добавляем время
+    return schedule
+
+
+def get_days_and_time():
+    # return {}
+    """Получаем дни недели и расписание на эти дни.
+    Возвращает словарь: {день недели: [расписание],...} для всех дней недели."""
+    box = None
+    while not box:
+        # Дождаться отображения списка дней
+        box = driver.find_elements(By.XPATH, f'/html/body/div[2]/div/div[2]/div[4]/div/div[2]/div[2]/div[1]')
+    days = box[0].find_elements(By.TAG_NAME, "button")  # Получить кнопки дней
+
+    # Нажать каждую кнопку пн, вт...
+    res = dict()
+    for day in days:
+        day.click()
+        sleep(0.3)
+        res[day.text] = get_schedule()  # Получаем расписание
+
+    return res
+
+def get_direction_and_bus_stop():
+    """Получаем направления движения (маршруты) и Списка остановок.
+    Возвращает словарь: {
+                            маршрут (конечные через тире): {
+                                остановка название: {
+                                    'id': идентификатор,
+                                    'direction': {расписание по дням}
+                                },
+                                ...
+                            },
+                            ...
+                        }
+    """
+    res = dict()  # Возвращаемый словарь
+    # Направления
+    directions = []
+    for i in range(5):
+        directions2 = driver.find_elements(By.XPATH, f'/html/body/div[2]/div/div[2]/div[4]/div/div[2]/h4[{i}]')
+        if directions2:
+            directions.append(directions2[0].text)
+    # return directions
+
+    # Списки остановок
+    string = 'ABCDEF'
+    for i, direction in enumerate(directions):
+        # Полный список остановок на маршруте
+        bus_stops = len(driver.find_elements(By.XPATH, f'//*[@id="trip{string[i]}"]/a[*]')) + 1
+        bus_stop_i = 1
+        # Перебираем остановки кликаем на каждой для открытия расписания
+        out = dict()  # Часть возвращаемого словаря
+        while bus_stops - bus_stop_i:
+            bus_stop = driver.find_elements(By.XPATH, f'//*[@id="trip{string[i]}"]/a[{bus_stop_i}]')
+            bus_stop_name = bus_stop[0].text
+            # print('   ', bus_stop[0].text, bus_stop_id)
+            bus_stop[0].click()
+            sleep(1)
+            bus_stop_id = driver.current_url.split('/')[-2]  # Уникальный номер остановки (id)
+            get = get_days_and_time()  # Вызов функции получения расписаний
+            bus_stop_i += 1
+            out[bus_stop_name] = {'id': bus_stop_id, 'direction': get}
+
+        res[direction] = out  # Заполняем данные для возврата
+
+    return res
+
+# Запускаем браузер
+driver = webdriver.Chrome()
+
+# Открываем сайт
+url = "https://gpmopt.by/mopt/Home/Index/sluck#/routes/bus"
+driver.get(url)
+sleep(2)
+
+# Получаем содержимое страницы
+page_source = driver.page_source
+
+# Находим номера маршрутов (номера автобусов) и записываем их в список
+direction_len = len(driver.find_elements(By.XPATH, '//*[@id="routeList"]/li[*]/a'))
+i = 0
+result = dict()  # Словарь с результатами работы
+while direction_len - i:
+    # Читаем найденные автобусы, получаем их номера.
+    # Кликаем поочереди даля открытия маршрутов автобуса
+    directions = driver.find_elements(By.XPATH, '//*[@id="routeList"]/li[*]/a')
+    bus_number = directions[i].text.split()[0]
+    if bus_number == '201С' or bus_number == '201C':
+        # Это останавливает процесс, чтобы не сканировать пригородные маршруты
+        break
+    # print('\nАвтобус', bus_number)
+    directions[i].click()
+    sleep(2)
+
+    result[bus_number] = get_direction_and_bus_stop()  # Функция читает маршруты
+
+    # final_station(d)  # Функция создает список конечных остановок
+    driver.get(url)  # Возврат к начальной странице и переход к следующему номеру
+    sleep(1)
+    i += 1
+    print(result)
+    save_result(result)  # Сохранение результата
+
+print(result)
