cb453f014937525dac7e63b27474d8d46d3f23f6
avdivo <avdivo@mail.ru>
2024-06-09 23:35:58 +0300

    Исправления Импорта (объединены остановки Молодежный центр)+

--- tbot/services/executors.py
+++ tbot/services/executors.py
@@ -256,7 +256,6 @@ class ExeAddBusStop(Executor):
             # Находим объекты остановок по названиям и направлению
             start_name = self.other_fields['start']
             bs_dict = BusStop.get_routers_by_two_busstop(start_name, self.key_name)
-            print(bs_dict)
             try:
                 # Сохраняем id начальной остановки
                 self.other_fields['start'] = bs_dict['start'].external_id
